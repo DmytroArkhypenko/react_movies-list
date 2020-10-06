3e88d1feb90c94a29ac279a39534e76e65beea6c
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-10-06 13:40:52 +0300

    Updates

---
+++ precache-manifest.2d3f62bf6b8043f111a0a5e2653fa019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8886d9fa87df20dde22842209436eff2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "68c1c7fbaea891c3ea29",
+    "url": "./static/css/main.4e9b7eda.chunk.css"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "./static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c1c7fbaea891c3ea29",
+    "url": "./static/js/main.88ac3f09.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.2d3f62bf6b8043f111a0a5e2653fa019.js"
 );
 
 self.addEventListener('message', (event) => {
